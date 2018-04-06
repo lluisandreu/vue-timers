ed1820e074d36f2939cbf693815e6804c167b3cc
Lluís Andreu Oliver Obrador <oliverobrador@gmail.com>
2018-04-07 00:27:42 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
+var uglify = require('gulp-uglify');
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/**/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+
+//script paths
+var jsFiles = 'src/js/**/*.js',
+    jsDest = 'dist/js';
+
+// Basic usage 
+gulp.task('scripts', function() {
+    // Single entry point to browserify 
+    gulp.src('src/js/app.js')
+        .pipe(browserify({
+          insertGlobals : true,
+          debug : !gulp.env.production
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/js'))
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/sass/**/*.scss', ['sass']);
+});
---
+++ src/js/app.js
@@ -0,0 +1,128 @@
+window.addEventListener('DOMContentLoaded', function () {
+    var Vue = require('vue');
+
+    new Vue({
+	  el: "#app",
+	  data: {
+	  	timers: [],
+	  },
+	  created: function() {
+	  	for (var i = 0; i < 4; i++) {
+	  		this.timers.push({
+		  		display: true,
+		  		times: [],
+			    animateFrame: 0,
+			    nowTime: 0,
+			    diffTime: 0,
+			    startTime: 0,
+			    isRunning: false,
+			    limit: 0,
+	  		});
+	  	};
+	  },
+	  watch: {
+	  	timers:  {
+	  		handler: function(timer) {
+	  			for (var i = 0; i < timer.length; i++) {
+	  				if(timer[i].isRunning) {
+	  					if(timer[i].diffTime > timer[i].limit) {
+	  						this.stopTimer(i);
+	  					}
+	  				}
+	  			};
+	  		}, 
+	  		deep: true
+	  	},
+	  },
+	  methods: {
+	    start: function(timer,min) {
+	    	if(min != 0) {
+	    		this.timers[timer].limit = min * 60 * 1000;
+	    	} else {
+	    		this.timers[timer].limit = 1800000;
+	    	}
+	    	this.timers[timer].startTime = 0;
+	    	this.startTimer(timer);
+	    },
+	    startAll: function(min) {
+	    	for (var i = 0; i < this.timers.length; i++) {
+	    		this.start(i,min);
+	    	};
+	    },
+	    setSubtractStartTime: function (timer, time) {
+	      var time = typeof time !== 'undefined' ? time : 0;
+	      this.timers[timer].startTime = Math.floor(performance.now() - time);
+	    },
+	    // タイマーをスタートさせる
+	    startTimer: function (timer) {
+	      // loop()内で this の値が変更されるので退避
+	      var vm = this.timers[timer];
+	      this.setSubtractStartTime(timer, vm.diffTime);
+	      // ループ処理
+	      (function loop(){
+	        vm.nowTime = Math.floor(performance.now());
+	        vm.diffTime = vm.nowTime - vm.startTime;
+	        vm.animateFrame = requestAnimationFrame(loop);
+	      }());
+	      vm.isRunning = true;
+	    },
+	    stopTimer: function (timer) {
+	      this.timers[timer].isRunning = false;
+	      cancelAnimationFrame(this.timers[timer].animateFrame);
+	    },
+	    stopAll: function() {
+	    	for (var i = 0; i < this.timers.length; i++) {
+	    		this.stopTimer(i);
+	    	};
+	    },
+	    pushTime: function (timer) {
+	    	var t = this.timers[timer];
+	      	t.times.push({
+		        hours: this.hours(timer),
+		        minutes: this.minutes(timer),
+		        seconds: this.seconds(timer),
+		        milliSeconds: this.milliSeconds(timer)
+	      	});
+	    },
+	    clear: function (timer) {
+	   		var t = this.timers[timer];
+	      	t.startTime = 0;
+	      	t.nowTime = 0;
+	      	t.diffTime = 0;
+	      	t.times = [];
+	     	this.stopTimer(timer);
+	      	t.animateFrame = 0;
+	    },
+	    clearAll: function() {
+	    	for (var i = 0; i < this.timers.length; i++) {
+	    		this.clear(i);
+	    	};
+	    },
+	    resetSim: function() {
+	    	for (var i = 0; i < this.timers.length; i++) {
+	    		this.timers[i].display = true;
+	    	};
+	    },
+	    hours: function (timer) {
+	      return Math.floor(this.timers[timer].diffTime / 1000 / 60 / 60);
+	    },
+	    minutes: function (timer) {
+	      return Math.floor(this.timers[timer].diffTime / 1000 / 60) % 60;
+	    },
+	    seconds: function (timer) {
+	      return Math.floor(this.timers[timer].diffTime / 1000) % 60;
+	    },
+	    milliSeconds: function (timer) {
+	      return Math.floor(this.timers[timer].diffTime % 1000);
+	    }
+	  },
+	  filters: {
+	    // ゼロ埋めフィルタ 引数に桁数を入力する
+	    // ※ String.prototype.padStart() は IEじゃ使えない
+	    zeroPad: function(value, num){
+	      var num = typeof num !== 'undefined' ? num : 2;
+	      return value.toString().padStart(num,"0");
+	    }
+	  }
+	});
+});
